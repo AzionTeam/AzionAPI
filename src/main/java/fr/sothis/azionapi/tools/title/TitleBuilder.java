ac041a74864c70458138a5d94500f8f7b1ba06dd
Sothis <sothis.inc04@gmail.com>
2022-05-18 18:28:48 +0200

    Update dependency craftbukkit

--- src/main/java/fr/sothis/azionapi/tools/title/TitleBuilder.java
+++ src/main/java/fr/sothis/azionapi/tools/title/TitleBuilder.java
@@ -1,11 +1,10 @@
 package fr.sothis.azionapi.tools.title;
 
-import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
-
 import net.minecraft.server.v1_8_R3.IChatBaseComponent;
 import net.minecraft.server.v1_8_R3.PacketPlayOutTitle;
+import org.bukkit.ChatColor;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.entity.Player;
 
 public class TitleBuilder {
 
